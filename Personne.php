f8c6d1e14a30cd7ff8f53b33b997cb8e00721a4a
aiemanuel <74431454+aiemanuel@users.noreply.github.com>
2024-11-29 07:15:40 -1000

    Personne(29/11)
    Personne modifié

--- Personne.php
+++ Personne.php
@@ -1,10 +1,28 @@
 <?php
     class Personne{
-        //Attributs
         private string $_nom, $_identifiant, $_mdp, $_email, $_numTel;
-        
-        //CONTRUCTEUR 
+      
         public function __construct($nomC, $id, $mdpC, $emailC,$numtelC){
+            if(!is_string($nomC)){
+                throw new InvalidArgumentException("Le nom doit etre une chaine de caractere");
+            }
+            
+            if(!is_string($id)){
+                throw new InvalidArgumentException("L'identifiant doit etre une chaine de caractere");
+            }
+
+            if(!is_string($mdpC)){
+                throw new InvalidArgumentException("Le mot de passe doit etre une chaine de caractere");
+            }
+
+            if(!is_string($emailC)){
+                throw new InvalidArgumentException("L'email doit etre une chaine de caractere");
+            }
+
+            if(!is_string($numtelC)){
+                throw new InvalidArgumentException("Le numero de téléphone doit etre une chaine de caractere");
+            }
+            
             $this->_nom = $nomC;
             $this->_identifiant = $id;
             $this->_mdp = $mdpC;
@@ -12,24 +30,60 @@
             $this->_numTel =$numtelC;
         }
 
-        //GET ET SET 
-        public function getNom(){return $this->_nom;}
-        public function getId(){return $this->_identifiant;}
-        public function getMdp(){return $this->_mdp;}
-        public function getEmail(){return $this->_email;}
-        public function getNumTel(){return $this->_numTel;}
+        public function getNom(){
+            return $this->_nom;
+        }
+        public function getId(){
+            return $this->_identifiant;
+        }
+        public function getMdp(){
+            return $this->_mdp;
+        }
+        public function getEmail(){
+            return $this->_email;
+        }
+        public function getNumTel(){
+            return $this->_numTel;
+        }
 
-        public function setNom($nom1){$this->_nom = $nom1;return;}
-        public function setId($id1){$this->_identifiant = $id1;return;}
-        public function setMdp($mdp1){$this->_mdp = $_mdp1;return;}
-        public function setEmail($email1){$this->_email= $email1;return;}
-        public function setNumTel($numTel1){$this->_numTel =$numTel1; return ;}
+        public function setNom($nom1){
+            if(!is_string($nom1)){
+                throw new InvalidArgumentException("Le nom doit etre une chaine de caractere");
+            }
+            $this->_nom = $nom1;
+        }
 
-        //Méthodes
+        public function setId($id1){
+            if(!is_string($id1)){
+                throw new InvalidArgumentException("L'identifiant doit etre une chaine de caractere");
+            }
+            $this->_identifiant = $id1;
+        }
 
-        public function verifPersonne(){
+        public function setMdp($mdp1){
+            if(!is_string($mdp1)){
+                throw new InvalidArgumentException("Le mot de passe doit etre une chaine de caractere");
+            }
+            $this->_mdp = $mdp1;
+        }
+        public function setEmail($email1){
+            if(!is_string($email1)){
+                throw new InvalidArgumentException("L'email doit etre une chaine de caractere");
+            }
+            $this->_email= $email1;
+        }
+
+        public function setNumTel($numTel1){
+            if(!is_string($numTel1)){
+                throw new InvalidArgumentException("Le numero de téléphone doit etre une chaine de caractere");
+            }
+            $this->_numTel =$numTel1;
+         }
+
+
+        public function verifPersonne($dbConnection){
             $stmt = $dbConnection->prepare("SELECT * FROM Personne WHERE identifiant = ?");
-            $stmt->execute([$id]);
+            $stmt->execute([$this->_identifiant]);
             return $stmt->rowCount() > 0;
         }
 
@@ -42,8 +96,6 @@
             }
         }
 
-        //voir s'il est utile, peut-etre avoir seulement modifier mot de passe ou identifiant,
-        //verifier s'il ne modifie pas par le meme, et le faire taper deux fois le mot de passe changer
         public function modifierInfoConnexion($id2,$mdp2){
             $this->setId($id2);
             $this->setMdp($mdp2);
