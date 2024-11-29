dd8d75cc48592c55984bb48c430bd9529a6f535f
aiemanuel <74431454+aiemanuel@users.noreply.github.com>
2024-11-29 07:17:58 -1000

    PaiementVirement(29/11)
    PaiementVirement modifié

--- PaiementVirement.php
+++ PaiementVirement.php
@@ -2,10 +2,23 @@
     class PaiementVirement{
 
         private float $_montant;
-        private date $_date;
+        private DateTime $_date;
         private RIB $_RibCrediteur, $_RibDebiteur;
 
         public function __construct($montant,$date, $RibC,$RibD){
+            if(!is_float($montant)){
+                throw new InvalidArgumentException("Le montant doit etre un float");
+            }
+            if(!$date instanceof DateTime){
+                throw new InvalidArgumentException("la date doit etre de type int");
+            }
+            if(!$RibD instanceof RIB ){
+                throw new InvalidArgumentException("Le rib du débiteur doit etre un Rib");
+            }
+            if(!$RibC instanceof RIB ){
+                throw new InvalidArgumentException("Le rib du créditeur doit etre un Rib");
+            }
+
              $this->_montant = $montant;
              $this->_date = $date;
              $this->_RibCrediteur = $RibC;
