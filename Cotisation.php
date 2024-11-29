1d3311149d18021417369738454650b892a49b9c
aiemanuel <74431454+aiemanuel@users.noreply.github.com>
2024-11-29 07:22:17 -1000

    Cotisation(29/11)
    Cotisation modifié

--- Cotisation.php
+++ Cotisation.php
@@ -1,5 +1,5 @@
 <?php
-    class Cotisation implements Paiement{
+    class Cotisation{
         private float $_montant;
         private DateTime $_datePaiement;
         private DateTime $_DateFin;
@@ -7,10 +7,29 @@
         public function __construct(){}
 
         public function initialiseCotisation($montant,$date){
+            if(!is_float($montant)){
+                throw new InvalidArgumentException("Le montant doit etre un float");
+            }
+            if(!$date instanceof DateTime){
+                throw new InvalidArgumentException("la date doit etre de type int");
+            }
             $this->_montant = $montant;
             $this->_datePaiement = $date;
             $this->_DateFin = clone $this->_datePaiement;
             $this->_DateFin-> modify('+1 year');
+        }
+        
+        public function getMontant(){
+            return $this->_montant;
+        }
+        public function getDateFin(){
+            return $this->_DateFin;
+        }
+        public function setMontant($montant){
+            if(!is_float($montant)){
+                throw new InvalidArgumentException("Le montant doit etre un float");
+            }
+            $this->_montant = $montant;
         }
         
         public function verifValiditeCotisation(){
@@ -18,7 +37,7 @@
             if($aujourdhui > $this->_DateFin){
                 return false;
             }
-            return true
+            return true;
         }
 
     }
