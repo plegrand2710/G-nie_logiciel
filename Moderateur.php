ee331274e830ce4a57c04966fd280344cb5a5ba3
aiemanuel <74431454+aiemanuel@users.noreply.github.com>
2024-11-29 07:20:21 -1000

    Moderateur(29/11)
    Moderateur modifié

--- Moderateur.php
+++ Moderateur.php
@@ -1,14 +1,29 @@
 <?php
-    //Cette classe va hériter de la classe Personne,un Moderateur est une Personne avec d'autres fonction en plus.
     class Moderateur extends Personne{
     
-        public function __construct(){
-            parent::construct();
+        public function __construct($nomC, $id, $mdpC, $emailC,$numtelC){
+            parent::__construct($nomC, $id, $mdpC, $emailC,$numtelC);
         }
 
-        public function supprimerutilisateur(){
-            //Supprime un utilisateur
-            //Donc une Personne?
+        public function supprimerutilisateur($id, $dbConnection) {
+            try {
+                $query = "DELETE FROM Utilisateur WHERE id = :id";
+                $stmt = $dbConnection->prepare($query);
+        
+                $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        
+                if ($stmt->execute()) {
+                    echo "Utilisateur avec l'ID $id a été supprimé avec succès.";
+                    return true;
+                } else {
+                    echo "Erreur lors de la suppression de l'utilisateur avec l'ID $id.";
+                    return false;
+                }
+            } catch (PDOException $e) {
+                echo "Erreur : " . $e->getMessage();
+                return false;
+            }
         }
+        
     }
 ?>
